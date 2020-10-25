f6c9713e1d7dd3a60dfeacc8c5c9df03d6bdad1d
jta1337 <ta_joshua@yahoo.com>
2020-10-24 22:55:17 -0700

    Created button class. Still needs text function

---
+++ kb_elements/Button.py
@@ -0,0 +1,53 @@
+from p5 import *
+
+#Button object for canvas. Called using, coordinates tuple, diameter, and character
+class Button:
+	def __init__(self, coordinates, diameter, character, font_name="arial.ttf"):
+		self.coordinates = coordinates
+		self.diameter = diameter
+		self.character = character
+		self.font_name = font_name
+
+	# Sets up button font. Put this in the setup() method
+	def setupButton(self):
+		arial = create_font(self.font_name, 10)
+		text_font(arial)
+		text_align(CENTER, CENTER)
+
+	# Calculates distance between button and mouse position, returns a float
+	def getDistFromKey(self):
+		self.dist_from_key = dist(self.coordinates, (mouse_x, mouse_y))
+		return self.dist_from_key
+
+	# Returns the diameter of the button as a float
+	def setDiameter(self, diameter):
+		self.diameter = diameter
+
+	# Sets a new position for the button
+	def setPosition(self, coordinates):
+		self.coordinates = coordinates
+
+	# Return the position of the button as a tuple
+	def getPosition(self):
+		return self.coordinates
+
+	# Returns the diameter of a button, usually a float
+	def getDiameter(self):
+		return self.diameter
+
+	# Draws the button on the screen. Put this in the draw() method
+	def drawButton(self):
+		no_stroke()
+		text_size(int((0.5)*self.diameter))
+
+		# If the mouse button is held down, fill with a lighter color
+		#TODO: Add text implementation
+		if mouse_is_pressed and (self.getDistFromKey() <= self.diameter/2):
+			fill(125)
+		else:
+			fill(155)
+		circle(self.coordinates, self.diameter)
+
+		# White text
+		fill(255)
+		text(self.character, self.coordinates)
