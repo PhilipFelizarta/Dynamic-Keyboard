8faf70c88d1a7c0754918c5a9b6490caa0b3aa4b
jta1337 <ta_joshua@yahoo.com>
2020-11-10 19:34:48 -0800

    Typing works, needs to correspond to buttons.

--- kb_elements/Keyboard.py
+++ kb_elements/Keyboard.py
@@ -1,22 +1,28 @@
 from Button import *
 from Display import *
 
+text_string = []
+
 def setup():
 	size(250, 400) #dimensions of a Galaxy S9
+	
 
 def draw():
-	
 	background(255) #white background for now
-	text_string = ""
+	string_join = ""
 
-	display = Display(20, 50, "Hello"); display.drawTextBox((10, 10), (230, 50)); display.drawText(text_string, (15, 45))
+	stroke(0)
+	circle((125, 250), 230)
 
-	button = Button((150, 300), 60, "a"); button.drawButton()
-	button2 = Button((50, 250), 30, "b"); button2.drawButton()
-	button3 = Button((170, 150), 45, "c"); button3.drawButton()
+	display = Display((10, 10)); display.drawTextBox((230, 50)); display.drawText(string_join.join(text_string), (15, 45))
 
-	button.type(text_string)
+	buttonA = Button((150, 300), 60, "a"); buttonA.drawButton()
+	buttonB = Button((50, 250), 30, "b"); buttonB.drawButton()
+	buttonC = Button((170, 200), 45, "c"); buttonC.drawButton()
 
+def mouse_released():
+	global text_string
+	text_string.append("a")
 
 if __name__ == '__main__':
 	run()
