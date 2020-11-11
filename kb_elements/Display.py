904178cbc85bcf143afb41c554362e6ef4f57417
jta1337 <ta_joshua@yahoo.com>
2020-11-10 18:20:21 -0800

    Display works, needs text from buttons.

--- kb_elements/Display.py
+++ kb_elements/Display.py
@@ -1,18 +1,18 @@
 from p5 import *
 
 class Display:
-  def __init__(self, coordinates, text_string, font_name="arial.ttf"):
+  def __init__(self, coordinates):
     self.coordinates = coordinates
     self.text_string = text_string
     self.font_name = font_name
-    #arial = create_font(self.font_name, 10)
-    #text_font(arial)
-    #text_align(LEFT, CENTER)
+    text_align("LEFT", "BOTTOM")
 
-  def add_string(self, text_string):
-    text(text_string)
-
-  def drawBox(self, coordinates, size):
+  def drawTextBox(self, coordinates, size):
     fill(255)
     stroke(0)
     rect(coordinates[0], coordinates[1], size[0], size[1])
+
+  def drawText(self, text_string, coordinates):
+    self.text_string = text_string
+    fill(0)
+    text(text_string, coordinates)
--- kb_elements/Keyboard.py
+++ kb_elements/Keyboard.py
@@ -1,4 +1,5 @@
 from Button import *
+from Display import *
 
 def setup():
 	size(250, 400) #dimensions of a Galaxy S9
@@ -6,10 +7,15 @@ def setup():
 def draw():
 	
 	background(255) #white background for now
+	text_string = ""
+
+	display = Display(20, 50, "Hello"); display.drawTextBox((10, 10), (230, 50)); display.drawText(text_string, (15, 45))
 
 	button = Button((150, 300), 60, "a"); button.drawButton()
 	button2 = Button((50, 250), 30, "b"); button2.drawButton()
 	button3 = Button((170, 150), 45, "c"); button3.drawButton()
+
+	button.type(text_string)
 
 
 if __name__ == '__main__':
