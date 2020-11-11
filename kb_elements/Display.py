e8f60a2b89e8cc096aa895ae5973d419a3d15269
jta1337 <ta_joshua@yahoo.com>
2020-11-10 19:35:16 -0800

    Updated display

--- kb_elements/Display.py
+++ kb_elements/Display.py
@@ -3,16 +3,14 @@ from p5 import *
 class Display:
   def __init__(self, coordinates):
     self.coordinates = coordinates
-    self.text_string = text_string
-    self.font_name = font_name
     text_align("LEFT", "BOTTOM")
 
-  def drawTextBox(self, coordinates, size):
+  def drawTextBox(self, size):
     fill(255)
     stroke(0)
-    rect(coordinates[0], coordinates[1], size[0], size[1])
+    rect(self.coordinates[0], self.coordinates[1], size[0], size[1])
 
   def drawText(self, text_string, coordinates):
     self.text_string = text_string
     fill(0)
-    text(text_string, coordinates)
+    text(self.text_string, coordinates)
