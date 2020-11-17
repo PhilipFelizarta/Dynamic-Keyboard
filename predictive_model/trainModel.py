5ffb6c12a615be742605f02013ba09f3f46d841e
Philip Felizarta <pfelizarta@gmail.com>
2020-11-17 15:31:40 -0800

    New parameter and architecture settings

--- predictive_model/trainModel.py
+++ predictive_model/trainModel.py
@@ -12,6 +12,8 @@ from tensorflow.keras.regularizers import l1, l2
 from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 
+from helper import *
+
 #Make results reproducible
 from numpy.random import seed
 seed(1)
@@ -22,6 +24,13 @@ set_random_seed(2)
 
 string_states = np.load("string_states.npy")
 labels = np.load("labels.npy")
+
+#Shuffle the Data
+np.random.seed(20) #Make this reproducible
+shuffle_indices = np.random.permutation(len(string_states))
+string_states = string_states[shuffle_indices]
+labels = labels[shuffle_indices]
+
 
 def create_fast(lr, lambd):
 	image = Input(shape=(10,26)) #String State Representation
@@ -42,19 +51,37 @@ def create_fast(lr, lambd):
 	model.summary() #Print a summary of the model
 	return model
 
-def create_slow(lr, lambd, filters=128): #Convolutional Neural Network
+def res_block(X, lr, lambd, filters=256):
+	#Convolutional Layers
+	channel = Conv1D(filters=filters, kernel_size=3, strides=1, 
+	kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="relu", padding="causal")(X)
+
+	channel = BatchNormalization(axis=-1)(channel)
+	channel = Conv1D(filters=filters, kernel_size=3, strides=1, 
+	kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="linear", padding="causal")(channel)
+
+	X = Add()([X, channel])
+	X = BatchNormalization(axis=-1)(X)
+	X = Activation("relu")(X)
+	return X
+
+
+def create_slow(lr, lambd, decay_rate, momentum, filters=128, block_num=10): #Convolutional Neural Network
 	image = Input(shape=(10,26)) #String State Representation
 
 	#Convolutional Layers
 	X = Conv1D(filters=filters, kernel_size=3, strides=1, 
-	kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="relu")(image)
+	kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="relu", input_shape=(10,26), padding="causal")(image)
 
-	X = Conv1D(filters=filters, kernel_size=3, strides=1, 
-	kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="relu")(X)
+	for _ in range(block_num):
+		X = res_block(X, lr, lambd, filters=filters)
 
 	latent = Flatten()(X)
 	latent = Dense(256, activation="relu", kernel_initializer=glorot_uniform(), 
 		kernel_regularizer=l2(lambd))(latent)
+	latent = Dense(128, activation="relu", kernel_initializer=glorot_uniform(), 
+		kernel_regularizer=l2(lambd))(latent)
+
 	p = Dense(26, activation="softmax", kernel_initializer=glorot_uniform(),
 		kernel_regularizer=l2(lambd))(latent)
 
@@ -62,12 +89,13 @@ def create_slow(lr, lambd, filters=128): #Convolutional Neural Network
 	model = Model(inputs=image, outputs=p)
 
 	#Compile model with our training hyperparameters
-	optimizer = tf.keras.optimizers.SGD(lr, momentum=0.9)
+	optimizer = tf.keras.optimizers.SGD(lr, momentum=momentum, decay=decay_rate, nesterov=True)
 	model.compile(loss="categorical_crossentropy", optimizer=optimizer, 
 		metrics=[tf.keras.metrics.TopKCategoricalAccuracy(k=5)])
 	
 	model.summary() #Print a summary of the model
 	return model
+
 
 def save_model(model, modelFile, weightFile): #Save model to json and weights to HDF5
 		from tensorflow.keras.models import model_from_json
@@ -77,9 +105,13 @@ def save_model(model, modelFile, weightFile): #Save model to json and weights to
 		model.save_weights(weightFile)
 		print("Model saved!")
 
-def train_model(lr, lambd, save=False, plot=False):
-	model = create_slow(lr, lambd)
-	loss_history = model.fit(string_states, labels, batch_size=64, epochs=50, validation_split=0.05, verbose=True)
+def train_model(lr, lambd, decay_rate, momentum, save=False, plot=False, load=True):
+	model = create_slow(lr, lambd, decay_rate, momentum)
+	if load:
+		start_param = load_model("slowmodel.json", "slowmodel.h5") #Import the trained model
+		model.set_weights(start_param.get_weights())
+
+	loss_history = model.fit(string_states, labels, batch_size=1024, epochs=7, validation_split=0.1, verbose=True)
 
 	if save:
 		save_model(model, "slowModel.json", "slowModel.h5") #Save model
@@ -106,7 +138,9 @@ def train_model(lr, lambd, save=False, plot=False):
 	return model, loss_history
 
 #Use hyper parameters found in parameterSearch
-lr = 0.004507324
-lambd = 1.44896e-09
+lr = 0.0316227766
+lambd = 2.5118864e-5
+decay = 3.1622766e-6
+momentum = 0.9
 
-model, loss_history = train_model(lr, lambd, save=True, plot=True)
+model, loss_history = train_model(lr, lambd, decay, momentum, save=True, plot=True, load=False)
