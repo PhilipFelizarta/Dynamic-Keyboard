c6a193c3c3ca5d0164ef755a6aae8481e07f32c5
Philip Felizarta <pfelizarta@gmail.com>
2020-11-20 17:16:44 -0800

    Optimize 6 parameters

--- predictive_model/parameterSearch.py
+++ predictive_model/parameterSearch.py
@@ -71,12 +71,12 @@ def res_block(X, lr, lambd, filters=256):
 	kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="linear", padding="causal")(channel)
 
 	X = Add()([X, channel])
-	X = BatchNormalization(axis=-1)(X)
 	X = Activation("relu")(X)
+	X = BatchNormalization(axis=-1)(X)
 	return X
 
 
-def create_slow(lr, lambd, decay_rate, momentum, filters=128, block_num=10): #Convolutional Neural Network
+def create_slow(lr, lambd, decay_rate, momentum, filters=256, block_num=25): #Convolutional Neural Network
 	image = Input(shape=(10,26)) #String State Representation
 
 	#Convolutional Layers
@@ -86,7 +86,9 @@ def create_slow(lr, lambd, decay_rate, momentum, filters=128, block_num=10): #Co
 	for _ in range(block_num):
 		X = res_block(X, lr, lambd, filters=filters)
 
-	latent = Flatten()(X)
+	latent = Conv1D(filters=32, kernel_size=1, strides=1,
+		kernel_initializer=glorot_uniform(), kernel_regularizer=l2(lambd), activation="relu", input_shape=(10,26), padding="causal")(X)
+	latent = Flatten()(latent)
 	latent = Dense(256, activation="relu", kernel_initializer=glorot_uniform(), 
 		kernel_regularizer=l2(lambd))(latent)
 	latent = Dense(128, activation="relu", kernel_initializer=glorot_uniform(), 
@@ -114,13 +116,22 @@ def save_model(model, modelFile, weightFile): #Save model to json and weights to
 		model.save_weights(weightFile)
 		print("Model saved!")
 
-def train_model(lr, lambd, decay_rate, momentum, save=False, plot=False, load=True):
-	model = create_slow(lr, lambd, decay_rate, momentum)
+def train_model(hyper_params, save=False, plot=False, load=True):
+	lr = hyper_params[0]
+	lambd = hyper_params[1]
+	decay_rate = hyper_params[2]
+	momentum = hyper_params[3]
+	filters = int(hyper_params[4])
+	blocks = int(hyper_params[5])
+
+	model = create_slow(lr, lambd, decay_rate, momentum, filters=filters, block_num=blocks)
 	if load:
 		start_param = load_model("slowmodel.json", "slowmodel.h5") #Import the trained model
 		model.set_weights(start_param.get_weights())
 
-	loss_history = model.fit(string_states, labels, batch_size=1024, epochs=7, validation_split=0.1, verbose=False)
+	#logdir="training_logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+	#tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+	loss_history = model.fit(string_states, labels, batch_size=1024, epochs=50, validation_split=0.1, verbose=False)
 
 	if save:
 		save_model(model, "slowModel.json", "slowModel.h5") #Save model
@@ -147,18 +158,18 @@ def train_model(lr, lambd, decay_rate, momentum, save=False, plot=False, load=Tr
 	return model, loss_history
 
 
+
 #Parameters of our Search
-iterations = 100 #How many parameters will we search before making a conclusion
+iterations = 50 #How many parameters will we search before making a conclusion
+exploration = 0.01
 
 #Put nonsense values
 hyper_params = []
 topk = []
 indices = [] #Save indices of used hyper params for plotting
 
-current_lr = 3e-3
-current_lambd = 1e-4
-current_decay = current_lr / 7
-current_momentum = 0.8
+#[lr, lambd, decay, moment, filters, resblocks]
+current_params = [3e-2, 1e-4, 1e-4, 0.85, 32, 1] 
 
 #Generate hyperparameter grid space we will optimize over.
 max_lr = 1e-1
@@ -172,6 +183,9 @@ min_decay = 1e-6
 
 max_momentum = 0.9
 min_momentum = 0.5
+
+filters = [32, 64, 128, 256, 512]
+resblocks = [1, 3, 5, 10]
 
 resolution = 10 #create a resxres grid of values to evaluate
 
@@ -189,11 +203,13 @@ for x in range(resolution):
 			decay = max_decay * np.power(decay_coeff, a)
 			for b in range(resolution):
 				momentum = min_momentum + b * (max_momentum - min_momentum) / (resolution - 1)
-				x_test.append([lr, lambd, decay, momentum])
+				for filt in filters:
+					for num_blocks in resblocks:
+						x_test.append([lr, lambd, decay, momentum, filt, num_blocks])
 
-x_test = np.reshape(x_test, [int(resolution*resolution*resolution*resolution), 4])
+x_test = np.reshape(x_test, [-1, len(current_params)])
 
-print("Testing: (", current_lr, ", ", current_lambd, ", ", current_decay, ", ", current_momentum, ")")
+print("Testing: ", current_params)
 
 #Training Loop
 plt.ion()
@@ -202,19 +218,19 @@ for it in range(iterations):
 
 	#Conduct k-fold CV to get an estimate of the generalization error of the model.
 
-	_ , history = train_model(current_lr, current_lambd, current_decay, current_momentum, save=False, plot=False, load=False) #Train a model using hypothesized best hyper params
+	_ , history = train_model(current_params, save=False, plot=False, load=False) #Train a model using hypothesized best hyper params
 
-	hyper_params.append([current_lr, current_lambd, current_decay, current_momentum]) #Append hyper params we used to train
+	hyper_params.append(current_params) #Append hyper params we used to train
 	topk.append(history.history["val_top_k_categorical_accuracy"][-1]) #Append final validation loss of model training
 	print("--Actual Accuracy--: ", history.history["val_top_k_categorical_accuracy"][-1])
 
-	if it > 1:
+	if it > 0:
 		#Create a gaussian process to model what hyperparams are the best
 		#kernel = gp.kernels.ConstantKernel(1.0, (1e-1, 1e3)) * gp.kernels.RBF(10.0, (1e-3, 1e3))
 		kernel = gp.kernels.ConstantKernel(1.0) * gp.kernels.Matern(length_scale=1.0, nu=2.5)
 		model = gp.GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha=0.1, normalize_y=True)
 
-		x_train = np.reshape(hyper_params, [-1, 4])
+		x_train = np.reshape(hyper_params, [-1, 6])
 		y_train = np.reshape(topk, [-1, 1])
 		model.fit(x_train, y_train)
 		mu, sigma = model.predict(x_test, return_std=True) #Make a prediction of the loss over param space
@@ -228,41 +244,35 @@ for it in range(iterations):
 		sigma = np.squeeze(sigma)
 		mu = np.squeeze(mu)
 
-		unbound_Z = (mu - optimum)/sigma
+		unbound_Z = (mu - optimum - exploration)/sigma
 		Z = np.where(sigma > 0, unbound_Z, 0) #Calculate Z for EI formula
-		unbound_EI = (mu - optimum)*norm.cdf(Z) + sigma*norm.pdf(Z)
+		unbound_EI = (mu - optimum - exploration)*norm.cdf(Z) + sigma*norm.pdf(Z)
 		EI = np.where(sigma > 0, unbound_EI, 0)
-		index = np.argmax(EI)
-		current_lr = x_test[index][0]
-		current_lambd = x_test[index][1]
-		current_decay = x_test[index][2]
-		current_momentum = x_test[index][3]
+		index = np.random.choice(np.flatnonzero(EI == np.max(EI))) #Randomly sample from the best.. if theres multiple
+		current_params = list(x_test[index])
 
 		indices.append(index)
 
 		#Plot our estimations and our acqusition function
 		axs[0].clear()
 		axs[1].clear()
-		axs[0].plot(mu, 'b--', lw=2, label="CV Accuracy")
-		axs[0].fill_between(np.arange(int(resolution*resolution*resolution*resolution))
+		axs[0].plot(mu, 'b--', lw=2, label="Estimated Accuracy")
+		axs[0].fill_between(np.arange(int(resolution*resolution*resolution*resolution*20))
 			, mu + sigma, mu - sigma, alpha=0.2)
 		axs[0].axhline(y=optimum, color="black", linestyle="--")
 		axs[0].plot(indices, topk, 'kx')
-		axs[0].legend()
+		axs[0].legend(loc="upper right")
 		axs[1].plot(EI, 'r', label="Expected Improvement")
 		axs[1].axvline(x=index, color="black", linestyle="--")
-		axs[1].legend()
+		axs[1].legend(loc="upper right")
 		plt.draw()
 		plt.pause(0.0001)
 
-		print("Testing: (", current_lr, ", ", current_lambd, ", ", current_decay, ", ", current_momentum, ")")
+		print("Testing: ", current_params)
 		print("Projected Accuracy: ", mu[index])
 	else: #If we don't have enough samples for the gaussian process, randomly sample from the gridspace
-		index = np.random.choice(int(resolution*resolution))
-		current_lr = x_test[index][0]
-		current_lambd = x_test[index][1]
-		current_decay = x_test[index][2]
-		current_momentum = x_test[index][3]
+		index = np.random.choice(int(resolution*resolution*resolution*resolution*20))
+		current_params = list(x_test[index])
 		indices.append(index)
 
 		#Plot our estimations and our acqusition function
@@ -271,11 +281,11 @@ for it in range(iterations):
 		axs[0].plot(indices, topk, 'kx', label="Samples")
 		axs[0].legend()
 		axs[1].axvline(x=index, color="black", linestyle="--", label="Next Sample")
-		axs[1].legend()
+		axs[1].legend(loc="upper right")
 		plt.draw()
 		plt.pause(0.0001)
 
-		print("Testing: (", current_lr, ", ", current_lambd, ", ", current_decay, ", ", current_momentum, ")")
+		print("Testing: ", current_params)
 		print("No Projected Accuracy (Random Sample)")
 
 	
@@ -286,7 +296,7 @@ best_params = hyper_params[best_index]
 print("Best Parameters: ", best_params)
 print("Sample Performance: ", np.max(topk))
 
-x_train = np.reshape(hyper_params, [-1, 4])
+x_train = np.reshape(hyper_params, [-1, 6])
 y_train = np.reshape(topk, [-1, 1])
 
 np.save("hyp_params", x_train)
